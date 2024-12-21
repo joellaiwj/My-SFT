7b15f5ff81248336fe62bd3be36dc9ebc97377fd
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2024-12-21 09:53:06 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -243,7 +243,7 @@ def yearly_plot_slope(data, min_year, max_year, color_palette):
         if min_year in yearly_values.index:
             min_value = yearly_values[min_year]
             fig.add_annotation(
-                x=min_year-0.1,
+                x=min_year-0.2,
                 y=min_value,
                 text=f"{domain} ({min_value:.2f})",
                 showarrow=False,
@@ -253,7 +253,7 @@ def yearly_plot_slope(data, min_year, max_year, color_palette):
         if max_year in yearly_values.index:
             max_value = yearly_values[max_year]
             fig.add_annotation(
-                x=max_year+0.1,
+                x=max_year+0.2,
                 y=max_value,
                 text=f"{domain} ({max_value:.2f})",
                 showarrow=False,
@@ -363,8 +363,8 @@ with st.sidebar:
     st.markdown("---")
 
     levels = ["Foundation", "Intermediate", "Advanced", "Graduate"]
-    default_levels = ["Foundation", "Intermediate", "Advanced"]
-    course_level = st.multiselect("**Course Level:**", options=levels, default=default_levels)
+    undergraduate = ["Foundation", "Intermediate", "Advanced"]
+    course_level = st.multiselect("**Course Level:**", options=levels, default=undergraduate)
 
     filtered_df = filter_data(df, years, subject_domain, course_level)
     filtered_SFT_courses = filter_data(SFT_courses, years, subject_domain, course_level)
@@ -372,8 +372,33 @@ with st.sidebar:
 tab_names = ["**Summary**", "**Detailed Reports**", "**Criteria Description**"]
 tabs = st.tabs(tab_names)
 
+st.markdown("""
+<style>
+
+	.stTabs [data-baseweb="tab-list"] p{
+		gap: 2px;
+        font-size: 35px;
+    }
+
+	.stTabs [data-baseweb="tab"] {
+		height: 50px;
+        white-space: pre-wrap;
+		background-color: #ffe8e8;
+		border-radius: 8px 8px 0px 0px;
+		gap: 1px;
+		padding: 10px;
+        
+    }
+
+	.stTabs [aria-selected="true"] {
+  		background-color: #ff2b2b;
+        color: white;
+        padding: 10px;
+	}
+
+</style>""", unsafe_allow_html=True)
+
 with tabs[0]:
-    st.header("Summary")
     # Summary Tab
     metrics = calculate_metrics(filtered_df, filtered_SFT_courses)
     overall_average_rating = calculate_average_scores(filtered_df,"Score","Overall")
@@ -421,7 +446,7 @@ with tabs[0]:
         select_metric = st.selectbox("",options=["SFT Score", "Favorability"])
         st.markdown("**SFT Score:** The average score attained by the faculty based on all student feedback ratings collected.")
         st.markdown("**Favorability:** The average score attained by the faculty based on all _positive_ student feedback ratings collected.")
-        st.markdown("\* The school favorability data is unavailable.")
+        st.markdown("\* The overall average favorability data is unavailable.")
     with col2_2:
         if select_metric == "SFT Score":
             semester_totals = calculate_average_scores(filtered_df,"Score","AcadYearSem")
@@ -474,7 +499,7 @@ with tabs[0]:
             x=df2_filtered["YearSemester"],  # Use filtered x-axis values
             y=df2_filtered["Overall"],
             mode="markers",  # Line with markers
-            name="Average School SFT Score*",
+            name="Overall Average Score",
             marker=dict(size=10, symbol="diamond", color='red'),
         )
 
@@ -482,82 +507,77 @@ with tabs[0]:
         st.plotly_chart(fig)
     
 with tabs[1]:
-    st.header("Detailed Reports")
-
-    subtab_names = ["**Criteria Report**", "**Course Report**"]
-    subtabs = st.tabs(subtab_names)
-
+    st.subheader("Criteria Report")
+    
     # Criteria Report Tab
-    with subtabs[0]:
-        # Multi-select for filtering
-        unique_criteria = SFT_criteria["Criteria Shortname"].dropna().unique()
-        default_criteria = ["Encouraged Engagement", "Communicated Clearly", "Supported Understanding", "Nurtured Critical Thinking", "Demonstrated Approachability"]
-        selected_criteria = st.multiselect("**Select Criterion:**", options=unique_criteria, default=default_criteria)
+    # Multi-select for filtering
+    unique_criteria = SFT_criteria["Criteria Shortname"].dropna().unique()
+    default_criteria = ["Encouraged Engagement", "Communicated Clearly", "Supported Understanding", "Nurtured Critical Thinking", "Demonstrated Approachability"]
+    selected_criteria = st.multiselect("**Select Criterion:**", options=unique_criteria, default=default_criteria)
 
-        min_year, max_year = year_range
+    min_year, max_year = year_range
 
-        # Filter data for criteria analysis
-        filtered_criteria_df = filter_data(filtered_df, years, subject_domain, course_level)
-        filtered_criteria_df = filtered_criteria_df[filtered_criteria_df["Criteria"].isin(selected_criteria)]
+    # Filter data for criteria analysis
+    filtered_criteria_df = filter_data(filtered_df, years, subject_domain, course_level)
+    filtered_criteria_df = filtered_criteria_df[filtered_criteria_df["Criteria"].isin(selected_criteria)]
 
-        # Calculate aggregates
-        average_scores, favor_scores = calculate_criteria_aggregates(filtered_criteria_df, min_year, max_year)
+    # Calculate aggregates
+    average_scores, favor_scores = calculate_criteria_aggregates(filtered_criteria_df, min_year, max_year)
 
-        # Generate a color palette for criteria
-        num_criteria = len(unique_criteria)
-        generated_colors = pc.qualitative.Dark24[:num_criteria]
-        dynamic_color_palette = {criteria: generated_colors[i] for i, criteria in enumerate(unique_criteria)}
+    # Generate a color palette for criteria
+    num_criteria = len(unique_criteria)
+    generated_colors = pc.qualitative.Dark24[:num_criteria]
+    dynamic_color_palette = {criteria: generated_colors[i] for i, criteria in enumerate(unique_criteria)}
 
-        # Check if data is available for the selected min_year and max_year
-        if min_year not in average_scores.index or max_year not in average_scores.index:
-            st.warning("Data not available for the selected start/end year.")
-        else:
-            fig1 = yearly_plot_slope(average_scores,year_range[0],year_range[1],dynamic_color_palette)
-            fig2 = yearly_plot_slope(favor_scores,year_range[0],year_range[1],dynamic_color_palette)
-        
-            with st.expander("**SFT Rating Evolution for Selected Criterion**"):
-                st.plotly_chart(fig1, use_container_width=True)
-            with st.expander("**Favorability Rating Evolution for Selected Criterion**"):
-                st.plotly_chart(fig2, use_container_width=True)   
+    # Check if data is available for the selected min_year and max_year
+    if min_year not in average_scores.index or max_year not in average_scores.index:
+        st.warning("Data not available for the selected start/end year.")
+    else:
+        fig1 = yearly_plot_slope(average_scores,year_range[0],year_range[1],dynamic_color_palette)
+        fig2 = yearly_plot_slope(favor_scores,year_range[0],year_range[1],dynamic_color_palette)
+    
+        with st.expander("**SFT Rating Evolution for Selected Criterion**"):
+            st.plotly_chart(fig1, use_container_width=True)
+        with st.expander("**Favorability Rating Evolution for Selected Criterion**"):
+            st.plotly_chart(fig2, use_container_width=True)   
 
-    # Course Report Tab
-    with subtabs[1]:
-        # Sorting and filtering courses
-        filtered_courses = filter_data(SFT_courses, years, subject_domain, course_level)
-        filtered_courses = filtered_courses.sort_values(by="CourseCode")
-        selected_course = st.selectbox("**Select a Course:**", (filtered_courses["CourseCode"] + " - " + filtered_courses["CourseName"]).unique())
+    st.subheader("Course Report")
+    # Sorting and filtering courses
+    filtered_courses = filter_data(SFT_courses, years, subject_domain, course_level)
+    filtered_courses = filtered_courses.sort_values(by="CourseCode")
+    selected_course = st.selectbox("**Select a Course:**", (filtered_courses["CourseCode"] + " - " + filtered_courses["CourseName"]).unique())
 
-        # Combine TutorialGrp and AcadYear into a unique identifier
-        df["TutorialGrp_Year"] = df["TutorialGrp"] + " (" + df["AcadYear"].astype(str).str.replace("\.0", "", regex=True) + ")"
+    # Combine TutorialGrp and AcadYear into a unique identifier
+    df["TutorialGrp_Year"] = df["TutorialGrp"] + " (" + df["AcadYear"].astype(str).str.replace("\.0", "", regex=True) + ")"
 
-        # Filter df based on the selected course
-        filtered_df = df[(df["CourseCode"] + " - " + df["CourseName"] == selected_course)]
+    # Filter df based on the selected course
+    filtered_df = df[(df["CourseCode"] + " - " + df["CourseName"] == selected_course)]
 
-        # Aggregate data for the selected course
-        aggregated_scores, aggregated_favor = calculate_course_aggregates(
-            df, selected_course, group_by_columns=["Criteria", "TutorialGrp_Year"]
-        )
-        
-        # Sort tutorials within each domain
-        aggregated_scores = sort_tutorials_within_domain(aggregated_scores)
-        aggregated_favor = sort_tutorials_within_domain(aggregated_favor)
-        
-        # Standard deviations for annotations
-        score_stdev = df.groupby(["Criteria"])["ScoreStdev"].mean()
-        favor_stdev = df.groupby(["Criteria"])["FavStdev"].mean()
+    # Aggregate data for the selected course
+    aggregated_scores, aggregated_favor = calculate_course_aggregates(
+        df, selected_course, group_by_columns=["Criteria", "TutorialGrp_Year"]
+    )
+    
+    # Sort tutorials within each domain
+    aggregated_scores = sort_tutorials_within_domain(aggregated_scores)
+    aggregated_favor = sort_tutorials_within_domain(aggregated_favor)
+    
+    # Standard deviations for annotations
+    score_stdev = df.groupby(["Criteria"])["ScoreStdev"].mean()
+    favor_stdev = df.groupby(["Criteria"])["FavStdev"].mean()
 
-        # Prepare indices and colors
-        indices = np.arange(len(aggregated_scores))
-        unique_tutorials = aggregated_scores.columns
-        colors = px.colors.qualitative.Pastel
+    # Prepare indices and colors
+    indices = np.arange(len(aggregated_scores))
+    unique_tutorials = aggregated_scores.columns
+    colors = px.colors.qualitative.Pastel
 
-        fig3 = plot_bar(aggregated_scores, score_stdev,colors)
-        fig4 = plot_bar(aggregated_favor, favor_stdev,colors)
+    fig3 = plot_bar(aggregated_scores, score_stdev,colors)
+    fig4 = plot_bar(aggregated_favor, favor_stdev,colors)
 
-        with st.expander("**SFT Rating by Tutorial for Selected Course**"):
-            st.plotly_chart(fig3, use_container_width=True)
-        with st.expander("**Favorability Rating by Tutorial for Selected Course**"):
-            st.plotly_chart(fig4, use_container_width=True)
+    with st.expander("**SFT Rating by Tutorial for Selected Course**"):
+        st.plotly_chart(fig3, use_container_width=True)
+    with st.expander("**Favorability Rating by Tutorial for Selected Course**"):
+        st.plotly_chart(fig4, use_container_width=True)
 
 with tabs[2]:
     st.table(SFT_criteria)
